04d5de477e0a4c3470ec228ccc8e98d1cdd8d9b6
RisingOrange <31575114+RisingOrange@users.noreply.github.com>
2020-09-13 19:57:49 +0200

    Fix not resetting decks with spaces

--- reset_ease.py
+++ reset_ease.py
@@ -11,7 +11,7 @@ def reset_ease():
     if not deck_to_user_ease:
         return
     for deck_id, user_ease in deck_to_user_ease.items():
-        card_ids = mw.col.find_cards(f'deck:{mw.col.decks.name(deck_id)}')
+        card_ids = mw.col.find_cards(f'deck:"{mw.col.decks.name(deck_id)}"')
         for card_id in card_ids:
             card = mw.col.getCard(card_id)
             if card.factor == user_ease_to_ease(user_ease):
