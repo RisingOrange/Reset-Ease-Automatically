fc694992a6f06b68f140a51f3d8baba3e0ec58cf
RisingOrange <31575114+RisingOrange@users.noreply.github.com>
2020-09-15 21:14:21 +0200

    Refactor preferences_dialog

---
+++ table_dialog.py
@@ -0,0 +1,199 @@
+from textwrap import dedent
+
+from anki.lang import _
+from aqt import mw
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+from .config import get_value, set_value
+from .utils import clean_up_deck_to_ease
+
+
+class TableDialog(QDialog):
+    def __init__(self, parent=None):
+        super(TableDialog, self).__init__(parent)
+
+        # initialize the table model and view
+        self._table_model, self._table_view = self._prepare_table_model_and_view()
+
+        clean_up_deck_to_ease()
+        if get_value('deck_to_ease'):
+            rows = [
+                {"Deck" : deck_id, "Ease" : ease}
+                for deck_id, ease in get_value('deck_to_ease').items()
+            ]
+            for row in rows:
+                self._append_row(row)
+
+        self.resize(500, 300)
+        self.setWindowTitle('Reset Ease - Preferences')
+        self.vbox = QVBoxLayout(self)
+
+        label = QLabel(dedent('''
+            For each deck listed, the Ease Factor will be reset to the specified value when Anki starts / the user profile is loaded.
+            '''
+        ).strip().replace('\n', ' '))
+
+        label.setWordWrap(True)
+        font = label.font()
+        font.setPixelSize(10)
+        label.setFont(font)
+        self.vbox.addWidget(label)
+
+        # add table
+        self.vbox.addWidget(self._table_view)
+
+        # add QHboxLlayout
+        hbox = QHBoxLayout()
+        self.vbox.addLayout(hbox)
+
+        # add buttons
+        self.add = make_button("Add", self._on_add, hbox)
+        self.delete = make_button("Delete", self._on_delete, hbox)
+        self.up = make_button("Up", self._on_up, hbox)
+        self.down = make_button("Down", self._on_down, hbox)
+
+        self._add_apply_and_cancel_buttons()
+
+        self.setLayout(self.vbox)     
+
+    def _prepare_table_model_and_view(self):
+        table_model = QStandardItemModel(0, 2)
+        table_view = QTableView()
+        table_view.setModel(table_model)
+        table_view.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        table_view.setSelectionBehavior(QAbstractItemView.SelectRows)
+        table_view.setSelectionMode(QAbstractItemView.SingleSelection)
+        table_model.setHeaderData(0, Qt.Horizontal, "Deck")
+        table_model.setHeaderData(1, Qt.Horizontal, "Ease")
+        return table_model, table_view
+
+    def _add_apply_and_cancel_buttons(self):
+        hbox = QHBoxLayout()
+        self.vbox.addLayout(hbox)
+
+        buttonCancel = QPushButton("&Cancel")
+        hbox.addWidget(buttonCancel, 1, Qt.AlignRight)
+        buttonCancel.setMaximumWidth(150)
+        buttonCancel.clicked.connect(self._on_cancel)
+
+        buttonOkay = QPushButton("&Apply")
+        hbox.addWidget(buttonOkay)
+        buttonOkay.setMaximumWidth(150)
+        buttonOkay.clicked.connect(self._on_okay)
+
+    def _on_cancel(self):
+        self.close()
+
+    def _on_okay(self):
+        deck_to_ease = {
+            row_data['Deck'] : row_data['Ease']
+            for row_data in self._rows()
+        }
+        
+        set_value('deck_to_ease', deck_to_ease)
+        self.close()
+
+    def _on_add(self):
+        data = {"Deck" : int(mw.col.decks.allIds()[0]), "Ease" : 250}
+        self._append_row(data)
+
+    def _on_delete(self):
+        if len(self._rows()) == 0:
+            return
+        
+        row_to_delete = self._current_row_idx()
+        self._table_model.removeRow(row_to_delete)
+
+    def _on_up(self):
+        row_idx = self._current_row_idx()
+        self._move_row_up(row_idx)
+        self._table_view.selectRow(row_idx - 1)
+
+    def _on_down(self):
+        # moving a row down means moving the next row up
+        row_idx = self._current_row_idx()
+        self._move_row_up(row_idx + 1)
+        self._table_view.selectRow(row_idx + 1)
+
+
+    def _current_row_idx(self):
+        indexes = self._table_view.selectedIndexes()
+        return 0 if len(indexes) == 0 else indexes[0].row()
+
+
+    def _rows(self):
+        # reads the data from the widgets in the table and returns it
+
+        assert hasattr(self, "_table_view")
+
+        def gui_row_to_data_row(gui_row):
+            result = {}
+            result['Deck'] = mw.col.decks.id(gui_row[0].currentText(), create=True)
+            result['Ease'] = int(gui_row[1].text())
+            return result
+
+        def gui_row_at_idx(row_idx):
+            return [
+                self._table_view.indexWidget(self._table_model.index(row_idx, col_idx))
+                for col_idx in range(self._table_view.model().columnCount())
+            ]
+
+        return [
+            gui_row_to_data_row(gui_row_at_idx(row_idx))
+            for row_idx in range(self._table_view.model().rowCount())
+        ]
+
+    def _set_row(self, row_idx, row_data):
+        # creates a gui row from the data and updates the _table_view with it
+        assert row_idx >= 0
+
+        def data_row_to_gui_row():
+
+            def prepare_deck_combo_box():
+                result = QComboBox()
+
+                deck_names = mw.col.decks.allNames()
+                result.addItems(deck_names)
+
+                name_of_selected_deck = mw.col.decks.name(row_data['Deck'])
+                result.setCurrentText(name_of_selected_deck)
+                return result
+
+            result = {}
+            result['deckComboBox'] = prepare_deck_combo_box()
+            result['easeEntry'] = QLineEdit(str(row_data['Ease']))
+            return result
+
+        def set_column(col_idx, widget):
+            self._table_view.setIndexWidget(self._table_model.index(row_idx, col_idx), widget)
+
+        gui_row = data_row_to_gui_row()
+        set_column(0, gui_row['deckComboBox'])
+        set_column(1, gui_row['easeEntry'])
+
+    def _append_row(self, row_data):
+        num_rows = len(self._rows())
+        self._table_model.setRowCount(num_rows + 1)
+        self._set_row(num_rows, row_data)
+
+    def _move_row_up(self, row_idx):
+
+        # the first "less than" is because the first row can't be moved up
+        if not 0 < row_idx < len(self._rows()):
+            return
+
+        # row at row_idx swaps place with row above
+        upper_row = self._rows()[row_idx - 1]
+        lower_row = self._rows()[row_idx]
+
+        self._set_row(row_idx - 1, lower_row)
+        self._set_row(row_idx, upper_row)
+
+
+def make_button(txt, f, parent):
+    b = QPushButton(txt)
+    b.clicked.connect(f)
+    parent.addWidget(b)
+    return b
