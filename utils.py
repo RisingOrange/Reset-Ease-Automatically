e40a169ff9db0d80973f28658903c9da0bdd5c6b
RisingOrange <31575114+RisingOrange@users.noreply.github.com>
2020-09-16 00:14:34 +0200

    Drop backwards compatibility: decks stored as names

--- utils.py
+++ utils.py
@@ -8,23 +8,9 @@ def clean_up_deck_to_ease():
     if not get_value('deck_to_ease'):
         return
 
-    #for backwards-compatibility (previously names were stored instead of ids)
-    convert_deck_names_to_ids_in_deck_to_ease()
-
     cleaned = {
         deck_id : ease
         for deck_id, ease in get_value('deck_to_ease').items()
         if str(deck_id) in mw.col.decks.allIds()
     }
     set_value('deck_to_ease', cleaned)
-
-def convert_deck_names_to_ids_in_deck_to_ease():
-    converted = {
-        (
-            mw.col.decks.id(deck_name_or_id, create=False)
-            if deck_name_or_id in mw.col.decks.allNames()
-            else deck_name_or_id
-        ) : ease
-        for deck_name_or_id, ease in get_value('deck_to_ease').items()
-    }
-    set_value('deck_to_ease', converted)
