a2bf94628e172418bc9a79c52a8e5ffb9142b234
RisingOrange <jakub.fidler@protonmail.com>
2021-05-07 23:46:14 +0200

    Fix config.py

--- src/Reset Ease Automatically/config.py
+++ src/Reset Ease Automatically/config.py
@@ -1,21 +1,23 @@
 from aqt import mw
 
-config = mw.addonManager.getConfig(__name__)
-
 
 def get(key):
+    config = mw.addonManager.getConfig(__name__)
     try:
         return config[_current_profile_name()][key]
     except KeyError:
         return None
 
+
 def set(key, value):
-    if config.get(_current_profile_name(), None) is not None: 
+    config = mw.addonManager.getConfig(__name__)
+    if config.get(_current_profile_name(), None) is not None:
         config[_current_profile_name()][key] = value
     else:
-        config[_current_profile_name()] = {key : value}
+        config[_current_profile_name()] = {key: value}
 
     mw.addonManager.writeConfig(__name__, config)
+
 
 def _current_profile_name():
     return mw.pm.name
