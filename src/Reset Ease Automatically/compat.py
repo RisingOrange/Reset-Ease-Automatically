654355944237f577e346ceee8f6c63be505a3303
RisingOrange <jakub.fidler@protonmail.com>
2022-11-15 14:06:32 +0100

    fix: bug  in compatibility alias code that caues incompatibilities with some addons

--- src/Reset Ease Automatically/compat.py
+++ src/Reset Ease Automatically/compat.py
@@ -3,7 +3,7 @@ from aqt import mw
 
 
 def add_compat_alias(namespace, new_name, old_name):
-    if new_name not in list(namespace.__dict__.keys()):
+    if new_name not in dir(namespace):
         setattr(namespace, new_name, getattr(namespace, old_name))
         return True
 
@@ -14,16 +14,16 @@ def setup_compat_aliases():
     add_compat_alias(mw.col, "get_card", "getCard")
     add_compat_alias(mw.col, "find_cards", "findCards")
 
-    if "all_names" not in DeckManager.__dict__.keys():
-        if "allNames" in DeckManager.__dict__.keys():
+    if "all_names" not in dir(DeckManager):
+        if "allNames" in dir(DeckManager):
             DeckManager.all_names = DeckManager.allNames
         else:
             DeckManager.all_names = lambda _: [
                 name for name, _ in DeckManager.all_names_and_ids()
             ]
 
-    if "all_ids" not in DeckManager.__dict__.keys():
-        if "allIds" in DeckManager.__dict__.keys():
+    if "all_ids" not in dir(DeckManager):
+        if "allIds" in dir(DeckManager):
             DeckManager.all_ids = DeckManager.allIds
         else:
             DeckManager.all_ids = lambda _: [
