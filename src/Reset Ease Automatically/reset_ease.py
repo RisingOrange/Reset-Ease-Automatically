65e051101974a56fe644ce4190c788d07ae30320
RisingOrange <jakub.fidler@protonmail.com>
2023-08-15 22:01:20 +0200

    Only change ease of cards in revision and relearning state

--- src/Reset Ease Automatically/reset_ease.py
+++ src/Reset Ease Automatically/reset_ease.py
@@ -1,3 +1,4 @@
+from anki.consts import CARD_TYPE_RELEARNING, CARD_TYPE_REV
 from anki.lang import _
 from aqt import mw
 
@@ -15,12 +16,19 @@ def reset_ease():
         card_ids = mw.col.find_cards(f'deck:"{mw.col.decks.name(deck_id)}"')
         for card_id in card_ids:
             card = mw.col.get_card(card_id)
+
+            # Only reset ease for review and relearning cards because only those cards use the ease factor.
+            # Cards use learning steps when in the learning phase.
+            if card.type not in [CARD_TYPE_REV, CARD_TYPE_RELEARNING]:
+                continue
+
             if card.factor < ease_min:
                 card.factor = ease_min
                 card.flush()
             elif card.factor > ease_max:
                 card.factor = ease_max
                 card.flush()
+
 
 def user_ease_to_ease(user_ease):
     return user_ease * 10
